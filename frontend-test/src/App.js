3be8014c55878e68c11c6b89db52c4b1028e490b
hjcalderon10 <dev.hectorjcalderon@gmail.com>
2019-05-03 18:58:57 -0500

    like an mvp

--- frontend-test/src/App.js
+++ frontend-test/src/App.js
@@ -2,7 +2,7 @@ import React, {Fragment, useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Server } from './api/Server'
-import Container from 'react-bootstrap/Container';
+import {Container, Row, Col} from 'react-bootstrap/';
 
 function App() {
 
@@ -31,25 +31,56 @@ function App() {
     }
   }, [value])
 
+  //función de https://stackoverflow.com/questions/3665115/how-to-create-a-file-in-memory-for-user-to-download-but-not-through-server
+  const download = () => {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(`{"data":${JSON.stringify(record.data)}}`));
+    element.setAttribute('download', 'Results.json');
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  }
+
   const list = record.data
   return (
     <Container className="app">
       <h1 className="title">PhoneWord App</h1>
-      <row>
-        <p className="explanation"> En esta aplicación, deberás colocar un input numérico para encontrar
+      <Row>
+        <Col xs='8'>
+          <p className="explanation"> En esta aplicación, deberás colocar un input numérico para encontrar
            todas las posibles combinaciones que este pueda tener dentro de un teclado como el que se muestra
             en la imagen.</p>
-        <img className="image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Telephone-keypad2.svg/800px-Telephone-keypad2.svg.png"
-          alt="Teclado de un celular no inteligente"/>
-        <p> Los valores que acepta el programa incluyen el 0 y el intervalo [2,9]</p>
-      </row>
-      
-      <br/>
-      <input type="number" placeholder="Ingrese un número" min="0" onChange={verifyInput}/>
-      {value !== '' ? <p className="realquery">El valor a consultar es {value}</p> : 
-        <p className="realquery">No hay un valor identificable dentro del campo</p>}
+          <p> Los valores que acepta el programa incluyen el 0 y el intervalo [2,9]. Son excluidos los demás
+            caracteres.</p>
+          <input type="number" placeholder="Ingrese un número" min="0" onChange={verifyInput}/>
+          {value !== '' ? <p className="realquery">El valor a consultar es {value}</p> : 
+          <p className="realquery">No hay un valor identificable dentro del campo</p>}
+        </Col>
+        <Col>
+          <img className="image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Telephone-keypad2.svg/800px-Telephone-keypad2.svg.png"
+          alt="Teclado de un celular no inteligente"/>  
+        </Col>
+      </Row>
       {list === undefined ? <p className="empty"> No hay resultados para mostrar </p> :
-        list.map((element,key) => <p key={key}> {element} </p>)}
+        <Fragment>
+          <p> La cantidad de resultados encontrados para "<span className="description">{value}</span>" 
+              fueron: <span className="result">{record.number}</span></p>
+          <button type="button" onClick={download}> Descargar resultados </button>
+          <Row className="results">
+            {list.map((element,key) => {
+              return(
+                <Col key={key}>
+                <p> {element} </p> 
+                </Col>
+              )
+            })}
+          </Row>
+        </Fragment>
+      }
     </Container>
   )   
   
